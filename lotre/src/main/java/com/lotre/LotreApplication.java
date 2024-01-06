5b8cfc85e1f727ca2bb91fb41d59973e40a71622
Baganda49 <abdessalemsaa@gmail.com>
2024-01-06 18:11:59 +0100

    create spring project

---
+++ lotre/src/main/java/com/lotre/LotreApplication.java
@@ -0,0 +1,13 @@
+package com.lotre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotreApplication.class, args);
+    }
+
+}
---
+++ lotre/src/test/java/com/lotre/LotreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
